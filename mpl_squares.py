63a8d7c8b382dbd60d51165d70e603c6e99973ef
Mirbekov <@kylych-bob>
2022-05-15 20:42:28 +0600

    diagramm

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,4 +1,21 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+squares = [1, 4, 9, 16, 25, 23, 22, 12, 13, 13, 11, 10]
 fig, ax = plt.subplots()
+# ax.plot(squares)
+# plt.show()
+
+# Настройка визуализации
+ax.plot(squares, linewidth=2)
+
+# Назначение заголовка диаграммы и меток
+ax.set_title('Square Numbers', fontsize=10)
+ax.set_xlabel('Value', fontsize=10)
+ax.set_ylabel('Square of Value', fontsize=10)
+
+# Назначение размера шрифта делений на осях
+ax.tick_params(axis='both', labelsize=10)
+
+plt.show()
+
+
